f5091fe510ade6a52d6d72ff81f4eb079fe95ecb
amiantos <bradroot@me.com>
2025-03-30 22:35:03 -0700

    add empty iOS project

--- Backend/source/main/config/__init__.py
+++ Backend/source/main/config/__init__.py
@@ -13,6 +13,6 @@ def create_app():
     app_instance.config["YOUR_AWESOME_CONFIG_ENV"] = "your awesome non sensitive value"
     # app_instance.config["YOUR_SUPER_SECRET_VALUE"] = os.getenv(
     #     "FAKE_SUPER_SECRET")
-    
+
     app_instance.register_blueprint(auth.auth_routes, url_prefix="/api/auth")
     return app_instance
