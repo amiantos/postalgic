aa3ae0026ee33fce3216e1f19aa9de64a105d527
amiantos <bradroot@me.com>
2025-05-10 11:17:26 -0700

    fix image sizing issues

--- Apps/iOS/Postalgic/Services/Site Generation/TemplateManager.swift
+++ Apps/iOS/Postalgic/Services/Site Generation/TemplateManager.swift
@@ -1055,13 +1055,22 @@ class TemplateManager {
             left: 0;
         }
 
-        .gallery-slide img {
+        .gallery-slide a {
             position: absolute;
             width: 100%;
             height: 100%;
-            object-fit: contain; /* Maintains aspect ratio while fitting in the container */
+            display: flex;
+            align-items: center;
+            justify-content: center;
             top: 0;
             left: 0;
+        }
+
+        .gallery-slide img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: contain; /* Maintains aspect ratio while fitting in the container */
+            display: block;
         }
 
         .gallery-nav {
