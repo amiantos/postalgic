4b86e5e1e8d83d135222c379a53278719f20efd8
amiantos <bradroot@me.com>
2025-05-21 21:44:42 -0700

    change brad.postalgic.app to a redirect

--- Websites/brad.postalgic.app/lib/brad.postalgic.app-stack.ts
+++ Websites/brad.postalgic.app/lib/brad.postalgic.app-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { aws_s3 as s3 } from "aws-cdk-lib";
-import { aws_s3_deployment as s3deploy } from "aws-cdk-lib";
 import { aws_certificatemanager as acm } from "aws-cdk-lib";
 import { aws_cloudfront as cloudfront } from "aws-cdk-lib";
 import { aws_cloudfront_origins as origins } from "aws-cdk-lib";
@@ -16,9 +15,10 @@ export class BradPostalgicAppStack extends cdk.Stack {
     const PARENT_DOMAIN = "postalgic.app";
     const DOMAIN_NAME = `${SUBDOMAIN}.${PARENT_DOMAIN}`;
     const HOSTED_ZONE_ID = "Z0678296322S0NR3YLJXV";
+    const REDIRECT_TARGET = "https://ihavebeenfloated.org";
 
-    // Create an S3 bucket to host the static website
-    const bucket = new s3.Bucket(this, "StaticWebsiteBucket", {
+    // Create an S3 bucket configured for website redirect
+    const bucket = new s3.Bucket(this, "RedirectBucket", {
       bucketName: DOMAIN_NAME,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
@@ -29,14 +29,10 @@ export class BradPostalgicAppStack extends cdk.Stack {
         ignorePublicAcls: false,
         restrictPublicBuckets: false,
       }),
-      versioned: true,
-      websiteIndexDocument: "index.html",
-    });
-
-    // Deploy website files to S3 bucket
-    new s3deploy.BucketDeployment(this, "DeployWebsite", {
-      sources: [s3deploy.Source.asset("./temp_website")], // Path to the directory containing the index.html file
-      destinationBucket: bucket,
+      websiteRedirect: {
+        hostName: "ihavebeenfloated.org",
+        protocol: s3.RedirectProtocol.HTTPS,
+      },
     });
 
     // Create a reference to SSL certificate in ACM
@@ -44,12 +40,12 @@ export class BradPostalgicAppStack extends cdk.Stack {
       "arn:aws:acm:us-east-1:341089094749:certificate/70a54ae8-5855-4ffe-a2d0-830209c080c8";
     const certificate = acm.Certificate.fromCertificateArn(
       this,
-      "StaticSiteCertificate",
+      "RedirectCertificate",
       certificateArn
     );
 
     // Create a CloudFront distribution
-    const cdn = new cloudfront.Distribution(this, "StaticSiteCDN", {
+    const cdn = new cloudfront.Distribution(this, "RedirectCDN", {
       domainNames: [DOMAIN_NAME],
       defaultBehavior: {
         origin: new origins.HttpOrigin(bucket.bucketWebsiteDomainName, {
@@ -57,6 +53,7 @@ export class BradPostalgicAppStack extends cdk.Stack {
           httpPort: 80,
           httpsPort: 443,
         }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
         compress: true,
       },
@@ -66,20 +63,24 @@ export class BradPostalgicAppStack extends cdk.Stack {
     // Import the hosted zone
     const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, "HostedZone", {
       hostedZoneId: HOSTED_ZONE_ID,
-      zoneName: PARENT_DOMAIN, // Parent domain of your subdomain
+      zoneName: PARENT_DOMAIN,
     });
 
     // Create an A record for the subdomain
     new route53.ARecord(this, "SubdomainRecord", {
       zone: hostedZone,
-      recordName: SUBDOMAIN, // Just the subdomain part
+      recordName: SUBDOMAIN,
       target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cdn)),
       ttl: cdk.Duration.minutes(5),
     });
 
-    // Output the CloudFront domain name
-    new cdk.CfnOutput(this, "StaticSiteCDNDomain", {
+    // Output the CloudFront domain name and redirect target
+    new cdk.CfnOutput(this, "RedirectCDNDomain", {
       value: cdn.domainName,
+    });
+    
+    new cdk.CfnOutput(this, "RedirectTarget", {
+      value: REDIRECT_TARGET,
     });
   }
 }
