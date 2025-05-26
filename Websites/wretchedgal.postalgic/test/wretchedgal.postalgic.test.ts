43105390a9a627ee36e1d79bf14f1f7cb89769c1
amiantos <bradroot@me.com>
2025-05-25 21:03:04 -0700

    add wretchedgal.postalgic site

---
+++ Websites/wretchedgal.postalgic/test/wretchedgal.postalgic.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WretchedgalPostalgic from '../lib/wretchedgal.postalgic-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wretchedgal.postalgic-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WretchedgalPostalgic.WretchedgalPostalgicStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
