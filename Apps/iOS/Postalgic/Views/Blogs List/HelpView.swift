dfd93e8f50d856ef050c34d93af8838f60a2e3be
amiantos <bradroot@me.com>
2025-06-01 00:07:51 -0700

    slight fleshing out of the help screen

--- Apps/iOS/Postalgic/Views/Blogs List/HelpView.swift
+++ Apps/iOS/Postalgic/Views/Blogs List/HelpView.swift
@@ -11,23 +11,29 @@ struct HelpView: View {
     @Environment(\.dismiss) var dismiss
     var body: some View {
         NavigationStack {
-            ScrollView(.vertical) {
-                VStack {
-                    HelpTextView {
-                        Text("What is Postalgic?").font(.title3).bold().padding(.bottom, 6)
-                        Text("Postalgic is an all-in-one blogging client for your pocket. It allows you to create blogs, write posts, and then automatically generate the HTML for the site, and upload it to a host of your choosing. It's micro-blogging, or full blown blogging, fully decentralized in the way it should have always been for the past 25 years.")
+            List {
+                Section {
+                    VStack(alignment: .leading) {
+                        Text("What is Postalgic?").font(.title3).bold().padding([.top, .bottom], 8)
+                        Text("Postalgic is an all-in-one blogging client for your pocket. It allows you to create blogs, write posts, and then automatically generate the HTML for the site, and upload it to a host of your choosing. It's micro-blogging, fully decentralized in the way it should have always been for the past 25 years.").padding(.bottom, 8)
                     }
-                    HelpTextView {
-                        Text("Open Source").font(.title3).bold().padding(.bottom, 6)
-                        Text("Postalgic is open source, which means that if there's something you don't like about it, *you* have the power to change it. You can download the source code, make it better, and share your improvements with others. Neat, huh? At least, I think so. You can find [the source code on GitHub](https://github.com/amiantos/postalgic).")
+                }
+                Section {
+                    VStack(alignment: .leading) {
+                        Text("Open Source").font(.title3).bold().padding([.top, .bottom], 8)
+                        Text("Postalgic is open source, which means that if there's something you don't like about it, *you* have the power to change it. You can download the source code, make it better, and share your improvements with others. Neat, huh? At least, I think so. You can find [the source code on GitHub](https://github.com/amiantos/postalgic).").padding(.bottom, 8)
                     }
-                    HelpTextView {
-                        Text("Who built this?").font(.title3).bold().padding(.bottom, 6)
-                        Text("Postalgic was built by Brad Root, also known as Amiantos. He's built a number of great iOS apps, like...")
+                }
+                Section {
+                    VStack(alignment: .leading) {
+                        Text("Who built this?").font(.title3).bold().padding([.top, .bottom], 8)
+                        Text("Postalgic was built by Brad Root, also known as Amiantos. He's built a number of great iOS apps, you can view them [on the App Store](https://apps.apple.com/us/developer/brad-root/id1158641227).").padding(.bottom, 8)
                     }
-                    HelpTextView {
-                        Text("Need more help?").font(.title3).bold().padding(.bottom, 6)
-                        Text("It's easy to get in touch with me, if you have more questions: you can email me directly at bradroot@me.com, don't be shy!")
+                }
+                Section {
+                    VStack(alignment: .leading) {
+                        Text("Need more help?").font(.title3).bold().padding([.top, .bottom], 8)
+                        Text("It's easy to get in touch with me, if you have more questions: you can email me directly at bradroot@me.com, don't be shy!").padding(.bottom, 8)
                     }
                 }
             }
@@ -36,8 +42,7 @@ struct HelpView: View {
                     Button { dismiss() } label: { Text("Done") }
                 }
             }
-            .navigationTitle("Postalgic Help")
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("About")
         }
     }
 }
@@ -54,10 +59,5 @@ struct HelpTextView<Content: View>: View {
         VStack(alignment: .leading) {
             content()
         }
-        .padding(20)
-        .background(.fill)
-        .clipShape(RoundedRectangle(cornerRadius: 25.0))
-        .padding([.leading, .trailing, .bottom])
-        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
