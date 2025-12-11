3fbc2906f41edf3bb4250f584491c2d9c40e6d79
Brad Root <bradroot@me.com>
2025-12-11 14:14:20 -0800

    increase upload size limit

--- Apps/Self-Hosted/server/server.js
+++ Apps/Self-Hosted/server/server.js
@@ -46,8 +46,8 @@ if (needsMigration(DATA_ROOT)) {
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 // Make data root available to routes
 app.locals.dataRoot = DATA_ROOT;
